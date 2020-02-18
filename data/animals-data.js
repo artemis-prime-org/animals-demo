c7ff4fefcbce504f8d3dd2c1df15359eb167bfa5
artemis-prime <artemisprimedev@gmail.com>
2020-02-17 20:54:12 -0800

    doubled data

--- data/animals-data.js
+++ data/animals-data.js
@@ -89,4 +89,94 @@ export default [
     medium: require("../assets/medium/zebra.jpg"), 
     full: require("../assets/full/zebra.jpg") 
   },
+  { 
+    name: "alpaca too", 
+    thumb: require("../assets/thumb/alpaca.jpg"), 
+    medium: require("../assets/medium/alpaca.jpg"), 
+    full: require("../assets/full/alpaca.jpg") 
+  },
+  { 
+    name: "bee too", 
+    thumb: require("../assets/thumb/bee.jpg"), 
+    medium: require("../assets/medium/bee.jpg"), 
+    full: require("../assets/full/bee.jpg") 
+  },
+  { 
+    name: "butterfly too", 
+    thumb: require("../assets/thumb/butterfly.jpg"), 
+    medium: require("../assets/medium/butterfly.jpg"), 
+    full: require("../assets/full/butterfly.jpg") 
+  },
+  { 
+    name: "cat too", 
+    thumb: require("../assets/thumb/cat.jpg"), 
+    medium: require("../assets/medium/cat.jpg"), 
+    full: require("../assets/full/cat.jpg") 
+  },
+  { 
+    name: "cheetah too", 
+    thumb: require("../assets/thumb/cheetah.jpg"), 
+    medium: require("../assets/medium/cheetah.jpg"), 
+    full: require("../assets/full/cheetah.jpg") 
+  },
+  { 
+    name: "horse too", 
+    thumb: require("../assets/thumb/horse.jpg"), 
+    medium: require("../assets/medium/horse.jpg"), 
+    full: require("../assets/full/horse.jpg") 
+  },
+  { 
+    name: "hummingbird too", 
+    thumb: require("../assets/thumb/hummingbird.jpg"), 
+    medium: require("../assets/medium/hummingbird.jpg"), 
+    full: require("../assets/full/hummingbird.jpg") 
+  },
+  { 
+    name: "kookaburra too", 
+    thumb: require("../assets/thumb/kookaburra.jpg"), 
+    medium: require("../assets/medium/kookaburra.jpg"), 
+    full: require("../assets/full/kookaburra.jpg") 
+  },
+  { 
+    name: "lizard too", 
+    thumb: require("../assets/thumb/lizard.jpg"), 
+    medium: require("../assets/medium/lizard.jpg"), 
+    full: require("../assets/full/lizard.jpg") 
+  },
+  { 
+    name: "muskrat too", 
+    thumb: require("../assets/thumb/muskrat.jpg"), 
+    medium: require("../assets/medium/muskrat.jpg"), 
+    full: require("../assets/full/muskrat.jpg") 
+  },
+  { 
+    name: "owl too", 
+    thumb: require("../assets/thumb/owl.jpg"), 
+    medium: require("../assets/medium/owl.jpg"), 
+    full: require("../assets/full/owl.jpg") 
+  },
+  { 
+    name: "squirrel too", 
+    thumb: require("../assets/thumb/squirrel.jpg"), 
+    medium: require("../assets/medium/squirrel.jpg"), 
+    full: require("../assets/full/squirrel.jpg") 
+  },
+  { 
+    name: "swan too", 
+    thumb: require("../assets/thumb/swan.jpg"), 
+    medium: require("../assets/medium/swan.jpg"), 
+    full: require("../assets/full/swan.jpg") 
+  },
+  { 
+    name: "turtle too", 
+    thumb: require("../assets/thumb/turtle.jpg"), 
+    medium: require("../assets/medium/turtle.jpg"), 
+    full: require("../assets/full/turtle.jpg") 
+  },
+  { 
+    name: "zebra too", 
+    thumb: require("../assets/thumb/zebra.jpg"), 
+    medium: require("../assets/medium/zebra.jpg"), 
+    full: require("../assets/full/zebra.jpg") 
+  },
  ] 
